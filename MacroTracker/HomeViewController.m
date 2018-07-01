fcc6bf7ed59d75f28dc228956b5354277e7df42f
T. Molenaar <thijs.molenaar4@hva.nl>
2018-07-01 12:24:58 +0200

    Adds error explanation of crash.

--- MacroTracker/HomeViewController.m
+++ MacroTracker/HomeViewController.m
@@ -8,10 +8,12 @@
 
 #import "HomeViewController.h"
 
+
 @interface HomeViewController ()
 @property (weak, nonatomic) IBOutlet UIButton *setTargets;
 @property (weak, nonatomic) IBOutlet UIButton *unsetTargets;
 @property (weak, nonatomic) IBOutlet UILabel *targetsSetLabel;
+@property (strong) NSMutableArray *foods;
 
 @end
 
@@ -26,7 +28,50 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (NSManagedObjectContext *)managedObjectContext {
+    NSManagedObjectContext *context = nil;
+    id delegate = [[UIApplication sharedApplication] delegate];
+    if ([delegate performSelector:@selector(managedObjectContext)]) {
+        context = [delegate managedObjectContext];
+    }
+    return context;
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    
+    // view appears en wil objectcontext initialisen, maar error door niet bestaande app delegate
+    // functie managedObjectContext
+    
+    // Fetch the foods from persistent data store
+    NSManagedObjectContext *managedObjectContext = [self managedObjectContext];
+    NSFetchRequest *fetchRequest = [[NSFetchRequest alloc] initWithEntityName:@"Food"];
+    self.foods = [[managedObjectContext executeFetchRequest:fetchRequest error:nil] mutableCopy];
+    NSLog(@"abcd");
+}
+
 - (IBAction)setTargetsInDb {
+    NSManagedObjectContext *context = [self managedObjectContext];
+    
+    // Create a new managed object
+    NSManagedObject *newFood = [NSEntityDescription insertNewObjectForEntityForName:@"Food" inManagedObjectContext:context];
+    [newFood setValue:@"kefir/muesli/noten" forKey:@"name"];
+    [newFood setValue:@"21" forKey:@"protein"];
+    [newFood setValue:@"13" forKey:@"fat"];
+    [newFood setValue:@"65" forKey:@"carbs"];
+    [newFood setValue:@"13" forKey:@"fat"];
+    [newFood setValue:@"cup" forKey:@"serving_size_type"];
+    [newFood setValue:@"1" forKey:@"serving_size"];
+    
+    NSError *error = nil;
+    // Save the object to persistent store
+    if (![context save:&error]) {
+        NSLog(@"Can't Save! %@ %@", error, [error localizedDescription]);
+    }
+    
+    [self dismissViewControllerAnimated:YES completion:nil];
 }
 - (IBAction)unsetTargetsInDb {
 }
